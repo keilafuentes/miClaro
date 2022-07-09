4de8522dfd05e3033a85c0ad78d57e1760bac0e3
keilafuentes <luisdelgado@Luiss-MacBook-Pro.local>
2022-07-08 22:11:32 -0400

    added LoginTest class

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+
+public class LoginTest extends AppDriver{
+    @Test
+    public void login_test() throws MalformedURLException {
+        setUp();
+
+    }
+
+}
